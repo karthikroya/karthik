467ed150a7f5da42f0d60f746f16d93865b39001
karthikroya <89581263+karthikroya@users.noreply.github.com>
2021-08-26 16:25:05 +0530

    Add files via upload

---
+++ pos or neg.py
@@ -0,0 +1,8 @@
+num=float(input("enter the number:"))
+if num>=0:
+    if num==0:
+        print("you enter zero")
+    else:
+            print("the number is positive")
+else:
+                print("the number is negative")
